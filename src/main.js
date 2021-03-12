cc8e7f25c32726a569f56e4eedecc17cd5d54f1d
codexgh <1239425965@qq.com>
2021-03-12 11:06:55 +0800

    supermall

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+Vue.prototype.$bus = new Vue()
+
+new Vue({
+  render: h => h(App),
+  router
+}).$mount('#app')
+
+
